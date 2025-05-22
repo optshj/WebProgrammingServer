3689cc92b938e00569d5cfec6bb8d406104eab62
optshj <optshj@gmail.com>
2025-05-22 12:26:49 +0900

    [Fix] vercel 시간대 한국으로 고정

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
 import getWeather from "../services/getWeather";
 import getUV from "../services/getUV";
 import getAirPollution from "../services/getAirPollution";
@@ -7,11 +10,14 @@ import { generateChatResponse } from "../services/getGemini";
 
 const router = express.Router();
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 router.get("/data", async (req, res) => {
     try {
         const sidoName = (req.query.sidoName as string) || "인천";
         const cityName = (req.query.cityName as string) || "연수구";
-        const now = dayjs();
+        const now = dayjs().tz("Asia/Seoul");
         const [weatherData, uvData, airPollution] = await Promise.all([
             getWeather(),
             getUV(now),
